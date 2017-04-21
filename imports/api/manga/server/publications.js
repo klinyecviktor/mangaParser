0a4ea8e00265889c851a3e13e15c6d9f3b1f9341
Doctor <Vityok3458@gmail.com>
2017-04-21 16:36:14 +0300

    Sort

--- imports/api/manga/server/publications.js
+++ imports/api/manga/server/publications.js
@@ -2,6 +2,6 @@ import {Manga} from "../manga_collection"
 
 if (Meteor.isServer) {
     Meteor.publish('manga', function () {
-        return Manga.find();
+        return Manga.find().sort({seen: 1, lastUpdate: -1});
     })
 }
